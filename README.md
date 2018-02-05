c974648febf319f547ea2c86b45de30134efefd2
Adel Qalieh <aqalieh95@gmail.com>
2018-02-04 19:04:39 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# Convert Subdecks to Tag Hierarchy
+
+This Anki Add-on provides a menu entry to convert the currently active deck's
+subdecks into a tag hierarchy.
+
+To activate, restart Anki after installing, and open the parent deck you wish to
+convert. Click **Tools→Convert subdecks to tags** to merge all subdecks of the
+selected deck and add hierarchical tags.
+
+You may download this add-on by visiting [AnkiWeb](https://ankiweb.net/shared/info/1172858842),
+or by pasting the code `1172858842` into the Anki Add-on installation box
+through **Tools→Add-ons→Browse & Install**.
+
+## Reporting bugs
+
+To report bugs, visit the [GitHub issues](https://github.com/adelq/anki_hierarchy/issues)
+and describe your issue there with as much information as possible, like the
+error message you got and what you were trying to do. Thanks!
+
+## License
+
+This add-on was written by [Adel Qalieh](https://blog.adelqalieh.com/) and
+licensed under the GNU AGPL 3 or later. See the [LICENSE](LICENSE) file for more
+information.
---
+++ anki_hierarchy.py
@@ -0,0 +1,58 @@
+"""
+Convert Subdecks to Tag Hierarchy (Anki Add-on)
+
+Provides a new menu entry to convert the currently active deck's subdecks into
+a tag hierarchy.
+
+Copyright (c) 2017 Adel Qalieh <https://adelqalieh.com>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, either version 3 of the License, or any later
+version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+
+from aqt import mw
+from anki.utils import intTime, ids2str
+from aqt.qt import *
+
+
+def convert_subdecks_to_tags():
+    """Main function to convert currently selected deck."""
+    parent_deck_id = mw.col.decks.selected()
+    children_decks = mw.col.decks.children(parent_deck_id)
+    mw.checkpoint(_("convert subdeck to tags"))
+    for child_deck_name, child_deck_id in children_decks:
+        tag = child_deck_name
+
+        # Get old card properties
+        child_cids = mw.col.decks.cids(child_deck_id)
+        mod = intTime()
+        usn = mw.col.usn()
+        str_cids = ids2str(child_cids)
+
+        # Move cards to new deck
+        mw.col.db.execute(
+            "update cards set usn=?, mod=?, did=? where id in " + str_cids,
+            usn, mod, parent_deck_id
+        )
+        mw.col.decks.rem(child_deck_id)
+
+        # New tag based on child deck name
+        child_cards = (mw.col.getCard(cid) for cid in child_cids)
+        child_nids = set(c.nid for c in child_cards)
+        mw.col.tags.bulkAdd(list(child_nids), tag)
+    mw.requireReset()
+
+
+action = QAction("Convert subdecks to tags", mw)
+action.triggered.connect(convert_subdecks_to_tags)
+mw.form.menuTools.addAction(action)
